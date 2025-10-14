c8858cb65089b92573e8cba7590e4b5c27946822
Bright Mukonesi <48471360+Brightmuk@users.noreply.github.com>
2025-10-14 23:07:44 +0300

    target api 35

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 
         applicationId = "com.brimukon.kcatt"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
